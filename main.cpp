b2a3f494e94a77e921b1775ce607cff65840dcf7
jonathanavc <jonvenega@gmail.com>
2022-09-23 17:00:09 -0400

    🤨

--- main.cpp
+++ main.cpp
@@ -1,29 +1,36 @@
 #include <iostream>
+#include <chrono>
 #include "Avl.h"
 #include "Rbt.h"
+#include "Ubt.h"
+
 
 using namespace std;
 
+size_t n_elementos = (size_t)1<<20;
+
 int main(int argc, char const *argv[]){
-   
-    /*
-    AVL* _avl = new AVL();
 
-    for(int i = 0; i < 30; i++){
-        _avl->insert(i);
-        //_avl->printAVL();
-    }
-    _avl->printAVL();
-    */
+    srand(time(NULL));
 
-
+    UnbalancedTree* ubt = new UnbalancedTree();
+    AVL* avl = new AVL();
     RedBlackTree* rbt = new RedBlackTree();
 
-    for(int i = 0; i < 30; i++){
-        rbt->insert(i);
-    }
-    rbt->printRBT();
-    
+    auto start = std::chrono::system_clock::now();
+    for(size_t i = 0; i < n_elementos; i++) ubt->insert(i);
+    auto end = std::chrono::system_clock::now();
+    cout << "ubt Insert:"<< std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()/(double)(n_elementos) << endl;
+
+    start = std::chrono::system_clock::now();
+    for(size_t i = 0; i < n_elementos; i++) avl->insert(i);
+    end = std::chrono::system_clock::now();
+    cout << "avl Insert:"<< std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()/(double)(n_elementos) << endl;
+
+    start = std::chrono::system_clock::now();
+    for(size_t i = 0; i < n_elementos; i++) rbt->insert(i);
+    end = std::chrono::system_clock::now();
+    cout << "rbt Insert:"<< std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count()/(double)(n_elementos) << endl;
 
     return 0;
 }
